b31c852509e8c98b0734085fdff916be67dcc4c8
George <George-P-1@users.noreply.github.com>
2024-09-22 15:44:44 +0400

    Just Template

---
+++ main.py
@@ -0,0 +1,22 @@
+# Import libraries
+import yaml
+import json
+import soundfile as sf
+
+
+
+# SECTION Main code here
+
+def main() -> None:
+    None
+
+
+
+
+
+
+
+# !SECTION
+
+if __name__ == '__main__':
+    main()
