a318d47f5444cdeb5707463bf94bbee09f423928
George <George-P-1@users.noreply.github.com>
2024-09-24 18:29:44 +0400

    made path_data available in main function
    changed a setting of hydra in config.yaml

--- main.py
+++ main.py
@@ -5,27 +5,14 @@ from pathlib import Path
 import json
 import soundfile as sf
 
-# Functions
-
-@hydra.main(version_base=None, config_path=".", config_name="config")
-def load_conf(cfg: DictConfig):
-    print(cfg, "\n------\n")
-    print(cfg.test_path.root)
-    print(Path(cfg.test_path.root))
-    return cfg
-
 
 # SECTION Main code here
 
-def main() -> None:
-    # Get path data from config.yaml
-    path_data = load_conf()
-    print("----\n\n", path_data)
-    # print(path_data['test_path']['root'])
-
-
-
-
+@hydra.main(version_base=None, config_path=".", config_name="config")
+def main(path_data: DictConfig) -> None:
+    print(path_data, "\n------\n")
+    print(path_data.test_path.root)
+    print(Path(path_data.test_path.root))
 
 
 
