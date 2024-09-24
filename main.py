659883c26f0b7c466ccda4a2cb64fa301ec00439
George <George-P-1@users.noreply.github.com>
2024-09-24 18:08:14 +0400

    using hydra library to load config
    hydra runs in entry point of script so can't return cfg to main function. Need workaround.

--- main.py
+++ main.py
@@ -1,34 +1,27 @@
 # Import libraries
-import yaml
+import hydra
+from omegaconf import DictConfig
+from pathlib import Path
 import json
 import soundfile as sf
-import os
 
 # Functions
 
-def load_yaml(file_path):
-    """
-    Load data from YAML file
-    """
-    try:
-        with open(file_path, 'r') as file:
-            yaml_data = yaml.load(file, Loader=yaml.SafeLoader)
-            return yaml_data
-    except FileNotFoundError:
-        print(f'File not found: {file_path}')
-        return None
-    except yaml.YAMLError as error:
-        print(f'Error parsing YAML: {error}')
-        return None
+@hydra.main(version_base=None, config_path=".", config_name="config")
+def load_conf(cfg: DictConfig):
+    print(cfg, "\n------\n")
+    print(cfg.test_path.root)
+    print(Path(cfg.test_path.root))
+    return cfg
 
 
 # SECTION Main code here
 
 def main() -> None:
     # Get path data from config.yaml
-    path_data = load_yaml("config.yaml")
-    print(path_data)
-    print(path_data['test_path']['root'])
+    path_data = load_conf()
+    print("----\n\n", path_data)
+    # print(path_data['test_path']['root'])
 
 
 
