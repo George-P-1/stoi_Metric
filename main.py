c693deb400f8e183bbd392324d26457d2f642bfb
George <George-P-1@users.noreply.github.com>
2024-09-24 17:18:50 +0400

    python function to load config.yaml
    using yaml library

--- main.py
+++ main.py
@@ -2,13 +2,33 @@
 import yaml
 import json
 import soundfile as sf
+import os
 
+# Functions
+
+def load_yaml(file_path):
+    """
+    Load data from YAML file
+    """
+    try:
+        with open(file_path, 'r') as file:
+            yaml_data = yaml.load(file, Loader=yaml.SafeLoader)
+            return yaml_data
+    except FileNotFoundError:
+        print(f'File not found: {file_path}')
+        return None
+    except yaml.YAMLError as error:
+        print(f'Error parsing YAML: {error}')
+        return None
 
 
 # SECTION Main code here
 
 def main() -> None:
-    None
+    # Get path data from config.yaml
+    path_data = load_yaml("config.yaml")
+    print(path_data)
+    print(path_data['test_path']['root'])
 
 
 
