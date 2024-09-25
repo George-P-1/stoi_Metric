2011f95a87a13ad83831db5d83ccd71bda54532c
George <George-P-1@users.noreply.github.com>
2024-09-25 21:39:33 +0400

    coded paths to audio files to be opened

--- main.py
+++ main.py
@@ -16,18 +16,22 @@ def main(path_data: DictConfig) -> None:
         with open(path_data.test_path.ref_file, 'r') as ref_file:
             ref_json = json.load(ref_file)
             
-            # TODO - Make a automated way to open each spin and target anechoic scene using loop
+            # TODO - Make a automated way to open each spin and target anechoic scene using loop. Or maybe this script just needs to work on one prompt on each run.
             print(ref_json[0], "\n")    # REMOVE_LATER
-            spin_file_name_to_open = f"{ref_json[0]['signal']}.wav"         # REVIEW - can be named HA_output
-            print(spin_file_name_to_open, "\n") # REMOVE_LATER
-            target_anechoic_file_name_to_open = f"{ref_json[0]['scene']}_target_anechoic.wav"
-            print(target_anechoic_file_name_to_open, "\n")  # REMOVE_LATER
+            # TODO - NOTE - Path of audio files to open, HA_Output and target_anechoic
+            spin_file_path = Path(path_data.test_path.spin_folder) / f"{ref_json[0]['signal']}.wav"
+            print(spin_file_path, "\n")  # REMOVE_LATER
+            target_file_path = Path(path_data.test_path.scenes_folder) / f"{ref_json[0]['scene']}_target_anechoic.wav"
+            print(target_file_path, "\n")  # REMOVE_LATER
+            # TODO - NOTE - Opening audio files using soundfile
+            pass
 
-
-
+        ref_file.close()
     except FileNotFoundError:
         print(f'File not found: {path_data.test_path.ref_file}')
         return None
+    finally:
+        print(f'Finished processing JSON file.')
 
 
 
