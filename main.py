6a27b86098926c364e27da8417af32bd41dda188
George <George-P-1@users.noreply.github.com>
2024-09-26 16:07:06 +0400

    resampling of both spin and target

--- main.py
+++ main.py
@@ -5,8 +5,7 @@ from pathlib import Path
 import json
 import soundfile as sf
 from scipy.signal import resample
-import numpy as np
-import matplotlib.pyplot as plt
+
 
 # SECTION Main code here
 
@@ -28,20 +27,16 @@ def main(path_data: DictConfig) -> None:
             # TODO - NOTE - Opening audio files using soundfile
             spin, spin_sr = sf.read(spin_file_path)
             print(f"Number of samples of spin audio is {len(spin)} and sample rate is {ref_json[0]['signal']} is {spin_sr} and shape is {spin.shape}.")   # REMOVE_LATER
-            # print(f"sf.info() output: {sf.info(spin_file_path, True)}")   # REMOVE_LATER
-            # NOTE - Resampling 
+            target, target_sr = sf.read(target_file_path)
+            print(f"Number of samples of target audio is {len(target)} and sample rate is {ref_json[0]['scene']} is {target_sr} and shape is {target.shape}.")   # REMOVE_LATER
+            # NOTE - Resampling
+            new_sr = path_data.sample_rate
             # REVIEW - Can use scipy (resample or decimate functions) or librosa library. Some issue with scipy using only frequency domain or something like that.
-            spin_sr_resampled = path_data.sample_rate
-            spin_resampled = resample(spin, int(len(spin) * spin_sr_resampled / spin_sr))   # NOTE - current_no_of_samples / current_sampling_rate is the duration of audio signal
-            print(f"Number of samples of new spin audio is {len(spin_resampled)} and sample rate is {spin_sr_resampled} and shape is {spin_resampled.shape}.")  # REMOVE_LATER
-            # TODO - Plot the original and resampled audio using matplotlib
-            time_axis = np.arange(0, len(spin)) / spin_sr
-            print(f" Spin shape: {spin.shape} and time axis shape: {time_axis.shape}")
-            plt.stem(time_axis, spin[:,0]) # REMOVE_LATER - Only displaying one channel
-            time_axis = np.arange(0, len(spin_resampled)) / spin_sr_resampled
-            plt.stem(time_axis, spin_resampled[:,0])
-            plt.show()
-            # NOTE - realized it is a bad idea to graph it because there are 16000 samples per second. Maybe listening to it is a better idea.
+            spin_resampled = resample(spin, int(len(spin) * new_sr / spin_sr))   # NOTE - current_no_of_samples / current_sampling_rate is the duration of audio signal
+            print(f"Number of samples of new spin audio is {len(spin_resampled)} and sample rate is {new_sr} and shape is {spin_resampled.shape}.")  # REMOVE_LATER
+            target_resampled = resample(target, int(len(target) * new_sr / target_sr))
+            print(f"Number of samples of new target audio is {len(target_resampled)} and sample rate is {new_sr} and shape is {target_resampled.shape}.")  # REMOVE_LATER
+            # TODO - NOTE - Play audio files (before and after resampling)
 
         ref_file.close()
     except FileNotFoundError:
