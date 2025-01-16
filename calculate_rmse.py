Fixed RMSE calculation and moved to different script
Added some csv files containing results of runs.
And also true scores extracted from result JSON in CPC1 dataset.

calculate_rmse.py
@@ -5,11 +5,25 @@ import pandas as pd
 import numpy as np
 import json
 
-from mystoi import calc_RMSE 
+def calc_RMSE(stoi_arr, listeners_arr) -> float:
+    """
+    Calculates Root Mean Squared Error value when given two arrays.
+    Order of arguments doesn't matter in this function.
+    But remember to normalize before passing the arrays.
+    
+    Arguments:
+        stoi_arr (list[float] or np.ndarray): A list of STOI values
+        listeners_arr (list[float] or np.ndarray): A list of true intelligibility values from clarity JSON file
+
+    Returns:
+        float: RMSE value
+    """
+    error = stoi_arr - listeners_arr
+    return np.sqrt(np.mean((error) ** 2))
 
 @hydra.main(version_base=None, config_path=".", config_name="config")
 def main(path_data: DictConfig) -> None:
-    # SECTION - Open result JSON file
+    # SECTION - Extract true intelligibility/correctness from result JSON file
     try:
         with open(path_data.test_result_path.result_ref_file, 'r') as res_file:
             res_json = json.load(res_file)
@@ -21,7 +35,7 @@ def main(path_data: DictConfig) -> None:
                 true_scores.append(res_json[res_index]['correctness'])
 
             # Convert to numpy array
-            true_scores = np.array(true_scores)
+            true_scores = np.array(true_scores)/100
 
     except FileNotFoundError:
         raise Exception(f'JSON file not found: {path_data.test_result_path.result_ref_file}')
@@ -30,8 +44,15 @@ def main(path_data: DictConfig) -> None:
     #!SECTION
 
     # SECTION - Load Data from csv files 
-    mystoi_csv_path = "mystoi_scores_20250116_091620.csv"
-    pystoi_csv_path = "pystoi_scores_20250116_091620.csv"
+    # With 'Silent Frame Removal'
+    mystoi_csv_path = "mystoi_scores_2025-01-16_09-47-05.csv"
+    pystoi_csv_path = "pystoi_scores_2025-01-16_09-47-05.csv"
+    # Without 'Silent Frame Removal'
+    # mystoi_csv_path = "mystoi_scores_2025-01-16_10-37-23.csv"
+    # pystoi_csv_path = "pystoi_scores_2025-01-16_10-37-23.csv"
+    # Only 1000 samples
+    # mystoi_csv_path = "mystoi_scores_2025-01-16_17-25-56.csv"
+    # pystoi_csv_path = "pystoi_scores_2025-01-16_17-25-56.csv"
 
     # Import data frame
     mystoi_df = pd.read_csv(mystoi_csv_path)
@@ -44,11 +65,16 @@ def main(path_data: DictConfig) -> None:
     
     # Check for matching number of rows
     if len(mystoi_df) != len(true_scores):
-        raise ValueError("The number of rows in the computed STOI file and true scores JSON file must match.")
-    
+        if len(mystoi_df) < len(true_scores):
+            true_scores = true_scores[:len(mystoi_scores)]            
+        elif len(mystoi_df) > len(true_scores):
+            raise Exception("Number of rows in mystoi_scores is greater than the number of rows in true_scores.")
+
     #!SECTION
 
     # SECTION - Calculate RMSE
+    # Normalize so that true scores are between 0 and 1
+    true_scores = true_scores/100
     rmse_mystoi = calc_RMSE(mystoi_scores, true_scores)
     rmse_pystoi = calc_RMSE(pystoi_scores, true_scores)
 

main.py
@@ -31,7 +31,6 @@ def main(path_data: DictConfig) -> None:
             # SECTION - Loop over each scene in JSON file and compute STOI
             # Store STOI values for mystoi
             mystoi_scores = []  # List to hold STOI values
-            pystoi_scores = []  # List to hold pySTOI values
             for scene_index in range(len(ref_json)):
                 # scene_index = 1006 # for running only one scene
             
@@ -51,9 +50,7 @@ def main(path_data: DictConfig) -> None:
                 # Pad the shorter signal
                 if len(spin_resampled) < len(target_resampled): # pad spin
                     spin_resampled = np.pad(spin_resampled, (0, len(target_resampled) - len(spin_resampled)))
-                elif len(target_resampled) < len(spin_resampled): # pad target
-                    target_resampled = np.pad(target_resampled, (0, len(spin_resampled) - len(target_resampled)))
-                    print("Padding Target audio file.") # REVIEW - REMOVE_LATER maybe
+                # In CPC1 data, in the case of shorter signals, spin is always the shorter one. So no need to check whether to pad target.  
                 # !SECTION
                 
                 # SECTION - Convert stereo to mono and plot spectrograms and play audio
@@ -131,18 +128,12 @@ def main(path_data: DictConfig) -> None:
 
                 # !SECTION - End Loop
 
-                # SECTION - Compute STOI Metric using pystoi
-                # REMOVE_LATER
-                pystoi_val = pystoi.stoi(target_resampled_mono, spin_resampled_mono, new_sr)
-                # !SECTION
-
                 # SECTION - Implement STOI Metric
                 stoi_val = mystoi.compute_stoi(target_resampled_mono, spin_resampled_mono, new_sr)
                 # !SECTION
 
                 # Store computed STOI values to list
                 mystoi_scores.append(stoi_val)
-                pystoi_scores.append(pystoi_val)
 
                 # break  # for running only one scene
             #!SECTION
@@ -157,16 +148,11 @@ def main(path_data: DictConfig) -> None:
     #!SECTION
 
     # SECTION - Save mystoi_scores and true_scores to a csv file
-    # TODO - Save mystoi_scores and true_scores to a csv file and calculate RMSE in different script
+    # Save mystoi_scores and true_scores to a csv file and calculate RMSE in different script
     mystoi_output_file = f"mystoi_scores_{timestamp}.csv"
     mystoi_df = pd.DataFrame({"Scene_Index": range(len(mystoi_scores)), "STOI_Value": mystoi_scores})
     mystoi_df.to_csv(mystoi_output_file, index=False)  # Save without the index column
     print(f"mystoi_scores saved to {mystoi_output_file}")
-    # Save pystoi_scores to another CSV file
-    pystoi_output_file = f"pystoi_scores_{timestamp}.csv"
-    pystoi_df = pd.DataFrame({"Scene_Index": range(len(pystoi_scores)), "STOI_Value": pystoi_scores})
-    pystoi_df.to_csv(pystoi_output_file, index=False)  # Save without the index column
-    print(f"pystoi_scores saved to {pystoi_output_file}")
     #!SECTION
 
 