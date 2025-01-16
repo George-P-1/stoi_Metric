b7dde1ab1acc81bc125788c020a44c039f0181e3
George <George-P-1@users.noreply.github.com>
2025-01-16 21:02:28 +0100

    Removed redundant code
    calculate_rmse.py now imports true listener scores from csv file

--- calculate_rmse.py
+++ calculate_rmse.py
@@ -1,6 +1,4 @@
 # Import libraries
-import hydra
-from omegaconf import DictConfig
 import pandas as pd
 import numpy as np
 import json
@@ -10,7 +8,8 @@ def calc_RMSE(stoi_arr, listeners_arr) -> float:
     Calculates Root Mean Squared Error value when given two arrays.
     Order of arguments doesn't matter in this function.
     But remember to normalize before passing the arrays.
-    
+    Output is in the same normalized scale as the input arrays.
+
     Arguments:
         stoi_arr (list[float] or np.ndarray): A list of STOI values
         listeners_arr (list[float] or np.ndarray): A list of true intelligibility values from clarity JSON file
@@ -21,32 +20,12 @@ def calc_RMSE(stoi_arr, listeners_arr) -> float:
     error = stoi_arr - listeners_arr
     return np.sqrt(np.mean((error) ** 2))
 
-@hydra.main(version_base=None, config_path=".", config_name="config")
-def main(path_data: DictConfig) -> None:
-    # SECTION - Extract true intelligibility/correctness from result JSON file
-    try:
-        with open(path_data.test_result_path.result_ref_file, 'r') as res_file:
-            res_json = json.load(res_file)
-
-            # Add correctness scores to a list
-            true_scores = []
-
-            for res_index in range(len(res_json)):
-                true_scores.append(res_json[res_index]['correctness'])
-
-            # Convert to numpy array
-            true_scores = np.array(true_scores)/100
-
-    except FileNotFoundError:
-        raise Exception(f'JSON file not found: {path_data.test_result_path.result_ref_file}')
-    finally:
-        print(f'Finished processing Result JSON file.')
-    #!SECTION
-
+def main() -> None:
     # SECTION - Load Data from csv files 
     # With 'Silent Frame Removal'
     mystoi_csv_path = "mystoi_scores_2025-01-16_09-47-05.csv"
     pystoi_csv_path = "pystoi_scores_2025-01-16_09-47-05.csv"
+    true_scores_path = "true_listeners_scores.csv"
     # Without 'Silent Frame Removal'
     # mystoi_csv_path = "mystoi_scores_2025-01-16_10-37-23.csv"
     # pystoi_csv_path = "pystoi_scores_2025-01-16_10-37-23.csv"
@@ -57,12 +36,14 @@ def main(path_data: DictConfig) -> None:
     # Import data frame
     mystoi_df = pd.read_csv(mystoi_csv_path)
     pystoi_df = pd.read_csv(pystoi_csv_path)
+    true_scores_df = pd.read_csv(true_scores_path)
 
     # Extract columns
     scene_indices = mystoi_df['Scene_Index'].to_numpy()
     mystoi_scores = mystoi_df['STOI_Value'].to_numpy()
     pystoi_scores = pystoi_df['STOI_Value'].to_numpy()
-    
+    true_scores = true_scores_df['true_value'].to_numpy()
+
     # Check for matching number of rows
     if len(mystoi_df) != len(true_scores):
         if len(mystoi_df) < len(true_scores):
@@ -81,6 +62,8 @@ def main(path_data: DictConfig) -> None:
     # Print RMSEs
     print(f'MySTOI RMSE: {rmse_mystoi}')
     print(f'PySTOI RMSE: {rmse_pystoi}')
+    print(f'Mystoi RMSE [%]: {rmse_mystoi * 100:.2f}%')
+    print(f'PySTOI RMSE [%]: {rmse_pystoi * 100:.2f}%')
     #!SECTION
 
 
