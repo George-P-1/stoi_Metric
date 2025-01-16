ab68949f8414d607ceca79251bc51c8a9c0ac4a2
George <100698568+George-P-1@users.noreply.github.com>
2025-01-16 08:56:02 +0100

    Bugfix resampled length mismatch - fixed by padding (#2)
    Co-authored-by: George <George-P-1@users.noreply.github.com>

--- main.py
+++ main.py
@@ -18,7 +18,7 @@ import mystoi
 
 @hydra.main(version_base=None, config_path=".", config_name="config")
 def main(path_data: DictConfig) -> None:
-    print(path_data.test_path.ref_file) # REMOVE_LATER
+    # print(path_data.test_path.ref_file) # REMOVE_LATER
     # SECTION - Open reference JSON file
     try:
         with open(path_data.test_path.ref_file, 'r') as ref_file:
@@ -28,6 +28,7 @@ def main(path_data: DictConfig) -> None:
             # Store STOI values for mystoi
             mystoi_scores = []  # List to hold STOI values
             for scene_index in range(len(ref_json)):
+                # scene_index = 1006 # REMOVE_LATER - for running only one scene
             
                 # SECTION - Load and resample SPIN and clean audio 
                 # Path of audio files to open, HA_Output and target_anechoic
@@ -36,11 +37,28 @@ def main(path_data: DictConfig) -> None:
                 # Opening audio files using soundfile
                 spin, spin_sr = sf.read(spin_file_path)
                 target, target_sr = sf.read(target_file_path)
+                # print("Length of SPIN audio file:", len(spin)) # REMOVE_LATER
+                # print("Length of target audio file:", len(target)) # REMOVE_LATER
                 # Resampling
                 new_sr = path_data.sample_rate
                 # REVIEW - Can use scipy (resample or decimate functions) or librosa library. Some issue with scipy using only frequency domain or something like that.
                 spin_resampled = resample(spin, int(len(spin) * new_sr / spin_sr))   # NOTE - current_no_of_samples / current_sampling_rate is the duration of audio signal
                 target_resampled = resample(target, int(len(target) * new_sr / target_sr))
+                # if True:    # REMOVE_LATER
+                #     print("Sample rate of SPIN audio file:", spin_sr) # REMOVE_LATER
+                #     print("Sample rate of target audio file:", target_sr) # REMOVE_LATER
+                #     print("Length of resampled SPIN audio file:", len(spin_resampled)) # REMOVE_LATER
+                #     print("Length of resampled target audio file:", len(target_resampled)) # REMOVE_LATER
+                # Padding to make both signals of same length in case of different lengths
+                # Pad the shorter signal
+                if len(spin_resampled) < len(target_resampled): # pad spin
+                    spin_resampled = np.pad(spin_resampled, (0, len(target_resampled) - len(spin_resampled)))
+                elif len(target_resampled) < len(spin_resampled): # pad target
+                    target_resampled = np.pad(target_resampled, (0, len(spin_resampled) - len(target_resampled)))
+                    print("Padding Target audio file.") # REVIEW - REMOVE_LATER maybe
+                # if True:    # REMOVE_LATER
+                #     print("Length of resampled SPIN audio file:", len(spin_resampled)) # REMOVE_LATER
+                #     print("Length of resampled target audio file:", len(target_resampled)) # REMOVE_LATER
                 if False:    # REMOVE_LATER
                     print("Total number of scenes in JSON file:", len(ref_json))    # REMOVE_LATER
                     print("Info about Current SPIN File:\n", ref_json[scene_index], "\n")    # REMOVE_LATER
@@ -134,15 +152,16 @@ def main(path_data: DictConfig) -> None:
                     sd.play(target_resampled_mono, new_sr)
                     sd.wait()
 
-                # !SECTION
+                # !SECTION - End Loop
 
                 # SECTION - Compute STOI Metric using pystoi
+                # REMOVE_LATER
                 stoi_val = pystoi.stoi(target_resampled_mono, spin_resampled_mono, new_sr)
                 # print(f"STOI value for intelligibility of Mono SPIN signal {ref_json[scene_index]['signal']} is {stoi_val}.") # REMOVE_LATER
                 # !SECTION
 
                 # SECTION - Implement STOI Metric
-                stoi_val = mystoi.compute_stoi(target_resampled_mono, spin_resampled_mono, new_sr)
+                # stoi_val = mystoi.compute_stoi(target_resampled_mono, spin_resampled_mono, new_sr)
                 # print("----------mystoi values below----------\n") # REMOVE_LATER
                 # print(f"STOI value for intelligibility of Mono SPIN signal {ref_json[scene_index]['signal']} is {stoi_val}.") # REMOVE_LATER
                 # !SECTION
@@ -150,6 +169,7 @@ def main(path_data: DictConfig) -> None:
                 # Store STOI values for mystoi
                 mystoi_scores.append(stoi_val)
                 print("current scene index:", scene_index) # REMOVE_LATER
+                # break  # REMOVE_LATER - for running only one scene
             #!SECTION
 
         print(f"Number of scenes processed: {len(mystoi_scores)}") # REMOVE_LATER
@@ -179,7 +199,9 @@ def main(path_data: DictConfig) -> None:
     #!SECTION
 
     # SECTION - Calculate RMSE
-    mystoi.calc_RMSE(np.array(mystoi_scores), np.array(true_scores))
+    # TODO - Save mystoi_scores and true_scores to a csv file and calculate RMSE in different script
+    rmse_val = mystoi.calc_RMSE(np.array(mystoi_scores), np.array(true_scores))
+    print(f"RMSE value for STOI metric is {rmse_val}.")
     # !SECTION
 
 # !SECTION Main code ends here
